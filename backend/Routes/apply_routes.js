cc0cc9cf863fa4f8bbf11fb2eaf25e9705d440a4
baruah040506 <baruahayan2006@gmail.com>
2026-01-30 02:27:41 +0530

    delete: made new files  as lead instead of project

--- backend/Routes/apply_routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import authMiddleware from "../Middlewares/authMiddleware.js";
-import { applyToProject } from "../Controllers/apply_controller.js";
-
-const router = Router();
-
-router.post("/", authMiddleware, applyToProject);
-
-export default router;
